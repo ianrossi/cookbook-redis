690785946176ba4e2e38bd32ac0c346a98f734f2
Ian D. Rossi <irossi@visiblemeasures.com>
2013-08-23 16:09:11 -0400

    Fixed template again

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,8 @@ description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "1.0.0"
 
+depends					 "vmc-access"
+depends					 "vmc-yum"
+
 supports 'centos'
 supports 'amazon'
