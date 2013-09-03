d81b324124f75e2f07f493d3b12b9dbab5643277
Ian D. Rossi <irossi@visiblemeasures.com>
2013-09-03 13:18:25 -0400

    Removed overcommit execute block from redis recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,19 +7,19 @@
 # Install the redis package
 package "redis"
 
-sysctl = "/etc/sysctl.conf"
-vm_overcommit_found = ((dFile.read("#{sysctl}")) =~ /vm\.overcommit_memory/) != nil
-sysctl_text = '\n#Mod for redis\n#turn on memory over-commit\nvm.overcommit_memory=1\n'
+# sysctl = "/etc/sysctl.conf"
+# vm_overcommit_found = ((dFile.read("#{sysctl}")) =~ /vm\.overcommit_memory/) != nil
+# sysctl_text = '\n#Mod for redis\n#turn on memory over-commit\nvm.overcommit_memory=1\n'
 
-execute "uncomment-vm-overcommit" do
-  command     "sed -i 's/.*vm\.overcommit_memory.*/vm.overcommit_memory=1/g' #{sysctl} && sysctl vm.overcommit_memory=1"
-  only_if     { node[:redis][:vm_overcommit_memory] && vm_overcommit_found }
-end
+# execute "uncomment-vm-overcommit" do
+#   command     "sed -i 's/.*vm\.overcommit_memory.*/vm.overcommit_memory=1/g' #{sysctl} && sysctl vm.overcommit_memory=1"
+#   only_if     { node[:redis][:vm_overcommit_memory] && vm_overcommit_found }
+# end
 
-execute "append-vm-overcommit" do
-  command     "printf \"%b\" \"#{sysctl_text}\" >> #{sysctl} && sysctl vm.overcommit_memory=1"
-  only_if      { node[:redis][:vm_overcommit_memory] && !vm_overcommit_found }
-end
+# execute "append-vm-overcommit" do
+#   command     "printf \"%b\" \"#{sysctl_text}\" >> #{sysctl} && sysctl vm.overcommit_memory=1"
+#   only_if      { node[:redis][:vm_overcommit_memory] && !vm_overcommit_found }
+# end
 
 template "#{node["redis"]["conf_dir"]}redis.conf" do
   source    "redis.conf.erb"
